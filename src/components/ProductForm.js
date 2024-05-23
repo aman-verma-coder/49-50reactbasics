17a94e1ec77e67681a416141c741f6c3bbfc0e1a
Aman Verma <9330nama@gmail.com>
2024-05-23 16:59:40 +0530

    Handling Multiple State

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,29 +1,46 @@
 import { useState } from "react";
 import './ProductForm.css';
 function ProductForm() {
-    const [title, setTitle] = useState('');
-    const [date, setDate] = useState('');
-    function titleChangeHandler(event) {
+    const [newtitle, setTitle] = useState('');
+    const [newdate, setDate] = useState('');
+    // const [fullProductInput, setProductInput] = useState({
+    //     title: '',
+    //     date: ''
+    // });
+    function titleChangeHandler(event, prevState) {
+        // let obj = { ...prevState, title: event.target.value };
+        // console.log(obj);
+        // return obj;
         setTitle(event.target.value);
-        console.log(title);
+        // console.log(title);
     }
     function dateChangeHandler(event) {
         setDate(event.target.value);
-        console.log(date);
+        // console.log(date);
+    }
+    function onSubmitHandler(event) {
+        event.preventDefault();
+        const productData = {
+            title: newtitle,
+            date: newdate
+        }
+        console.log(productData);
     }
     return (
         <div className='newProduct'>
-            <div>
-                <label>Title:</label>
-                <input type='text' onChange={titleChangeHandler}></input>
-            </div>
-            <div>
-                <label>Date:</label>
-                <input type='date' min='01-01-2024' max='18-05-2024' onChange={dateChangeHandler}></input>
-            </div>
-            <div>
-                <button type='submit'>Add New Product</button>
-            </div>
+            <form onSubmit={onSubmitHandler}>
+                <div>
+                    <label>Title:</label>
+                    <input type='text' onChange={titleChangeHandler}></input>
+                </div>
+                <div>
+                    <label>Date:</label>
+                    <input type='date' min='01-01-2024' max='18-05-2024' onChange={dateChangeHandler}></input>
+                </div>
+                <div>
+                    <button type='submit'>Add New Product</button>
+                </div>
+            </form>
         </div>
     )
 }
